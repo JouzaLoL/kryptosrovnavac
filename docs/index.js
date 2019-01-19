58c67220df283914c1fc5b8ab03723dd2854820e
Josef Vacek <vacekj@outlook.com>
2019-01-19 18:40:26 +0100

    manual publish

---
+++ docs/index.js
@@ -0,0 +1,31 @@
+async function getSheetHTML() {
+    return fetch("https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/18gdu6Mi2vchsy9fS9B6QrYUKfwsMV9pVnCncUVkjdAY", {
+        mode: "cors",
+        referrer: "kryptosrovnavac.cz"
+    }).then(res => res.text());
+}
+
+function extractTableAndStylesFromHTML(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const table = doc.querySelector("tbody");
+    const styles = doc.querySelector("style:nth-child(5)");
+    return { table, styles };
+}
+
+function addStyles(styles) {
+    var node = document.createElement('style');
+    document.body.appendChild(node);
+    node.innerHTML = styles;
+}
+
+async function main() {
+    getSheetHTML()
+        .then(extractTableAndStylesFromHTML)
+        .then((result) => {
+            addStyles(result.styles);
+            document.querySelector("#burzy").appendChild(result.table);
+        });
+}
+
+// main();
